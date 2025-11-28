9cbbd7c6ab5e460a5df0be440c118ecbe97d521f
quidanghcm <quidanghcm@gmail.com>
2025-11-28 11:04:01 +0700

    update: build files + config production

--- config/server.js
+++ config/server.js
@@ -2,7 +2,10 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array('APP_KEYS', [
+      'key1_super_secret',
+      'key2_super_secret'
+    ]),
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
